ab538880f7caa38e3f24b314704a9a4a01f2a4fb
sattwikroy570 <sattwikroy@hotmail.com>
2022-07-09 12:10:13 +0530

    hope

--- Startup.cs
+++ Startup.cs
@@ -53,7 +53,6 @@ namespace CustomerMicroservice
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddLog4Net();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
